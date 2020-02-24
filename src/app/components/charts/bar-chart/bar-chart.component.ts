14116cb36ece72b3927ae1cbda87d8c29a786c7f
atheodosiou <anastasios.theodosiou@gmail.com>
2020-02-25 00:14:58 +0200

    Add second chart for kino bonus

--- src/app/components/charts/bar-chart/bar-chart.component.ts
+++ src/app/components/charts/bar-chart/bar-chart.component.ts
@@ -1,9 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import { ChartOptions, ChartType, ChartDataSets } from 'chart.js';
+import { ChartOptions, ChartType, ChartDataSets, ChartColor } from 'chart.js';
 import { Label } from 'ng2-charts';
 import { KinoService } from 'src/app/services/kino-service.service';
 import { NumberOccurrence, KinobonusOccurrence } from 'src/app/models/occcurrences.interface';
-// import * as pluginDataLabels from 'chartjs-plugin-datalabels';
+import * as pluginDataLabels from 'chartjs-plugin-datalabels';
+
 @Component({
   selector: 'bar-chart',
   templateUrl: './bar-chart.component.html',
@@ -23,12 +24,27 @@ export class BarChartComponent implements OnInit {
       }
     }
   };
+
+  public bonusChartOptions: ChartOptions = {
+    responsive: true,
+    scales: { xAxes: [{}], yAxes: [{}] },
+    plugins: {
+      datalabels: {
+        anchor: 'end',
+        align: 'end',
+      }
+    }
+  };
+
   public barChartLabels: Label[] = [];
-  public barChartType: ChartType = 'bar';
+  public barChartType: ChartType = 'bar'
+  public pieChartType: ChartType = 'bar'
+  bonusChartColors:ChartColor='blue';
   public barChartLegend = true;
-  // public barChartPlugins = [pluginDataLabels];
+  public barChartPlugins = [pluginDataLabels];
 
   public barChartData: ChartDataSets[];
+  public pieChartData: ChartDataSets[];
 
   constructor(private kinoService:KinoService) { }
 
@@ -40,31 +56,28 @@ export class BarChartComponent implements OnInit {
       console.log((res[0].body as NumberOccurrence),(res[1].body as KinobonusOccurrence))
       this.manipulateChartData(res[0].body as NumberOccurrence,res[1].body as KinobonusOccurrence);
     }).catch(error=>{console.log(error)});
-    // this.kinoService.getNumberOccurrences()
-    // .subscribe(res=>{
-    //   this.manipulateChartData(res.body as NumberOccurrence);
-    // },error=>{
-    //   console.log(error);
-    // });
   }
 
   private manipulateChartData(numberData:NumberOccurrence,kinobonusData:KinobonusOccurrence){
     if(!this.barChartData) this.barChartData=[];
+    if(!this.pieChartData) this.pieChartData=[];
     // = [
     //   { data: [65, 59, 80, 81, 56, 55, 40], label: 'Series A' },
     // ]
 
-    let numbers:ChartDataSets={label:'Αριθμός',data:[]};
-    let kinobonus:ChartDataSets={label:'Kinobonus',data:[]};
+    let numbers:ChartDataSets={label:'Αριθμός',data:[],backgroundColor:'#73C1FE',hoverBackgroundColor:'#20272A'};
+    let kinobonus:ChartDataSets={label:'Kinobonus',data:[],backgroundColor:'#EFB31D',hoverBackgroundColor:'#20272A'};
     
+    let percentageSum=0;
     numberData.occurences.forEach(oc=>{
-      numbers.data.push(oc.count);
+      numbers.data.push(oc.count);      
       this.barChartLabels.push(oc.number.toString());
     });
+    console.log(percentageSum)
     kinobonusData.occurences.forEach(oc=>{
       kinobonus.data.push(oc.count);
     });
     this.barChartData.push(numbers);
-    this.barChartData.push(kinobonus);
+    this.pieChartData.push(kinobonus);
   }
 }
